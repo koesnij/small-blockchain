f497e55d94b9b1e0d7eeb89e305d4f68a4cf0850
koesnij <m_jinseok@naver.com>
2020-09-13 21:06:49 +0900

    transaction

--- app/views/blockchain.py
+++ app/views/blockchain.py
@@ -4,6 +4,7 @@ from blockchain.blockchain import Blockchain
 
 blockchain = Blockchain()
 
+
 class Chain(Resource):
     def get(self):
         chains = blockchain.chain
@@ -13,10 +14,11 @@ class Chain(Resource):
             'length': len(chains)
         }
 
+
 class Mine(Resource):
     def get(self):
         last_block = blockchain.last_block
-        nonce = blockchain.proof_of_work(last_block['nonce'])   # Mine
+        nonce = blockchain.proof_of_work(last_block['nonce'])  # Mine
 
         blockchain.new_transaction(
             sender='0', recipient=node_id, amount=1
@@ -27,11 +29,20 @@ class Mine(Resource):
         new_block = blockchain.new_block(nonce, previous_hash)
 
         return {
-            'message': 'New Block Forged',
-            'block': {
-                'version': new_block['version'],
-                'transactions': new_block['transactions'],
-                'timestamp': new_block['timestamp'],
-                'nonce': new_block['nonce']
-            }
-        }, 200
+                   'message': 'New Block Forged',
+                   'block': {
+                       'version': new_block['version'],
+                       'transactions': new_block['transactions'],
+                       'timestamp': new_block['timestamp'],
+                       'nonce': new_block['nonce']
+                   }
+               }, 200
+
+
+class Transaction(Resource):
+    def post(self):
+        sender = request.form['sender']
+        recipient = request.form['recipient']
+        amount = request.form['amount']
+
+        blockchain.new_transaction(sender, recipient, amount)
